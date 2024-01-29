986b5d3c94e5356d45d64b40f4bd9f02b3347882
tarikjaber <tariknjaber@gmail.com>
2024-01-28 20:53:02 -0500

    initial version

--- main.go
+++ main.go
@@ -4,34 +4,41 @@ import (
     "fmt"
     "os"
     "os/exec"
+    "strconv"
     "time"
-    "net/http"
 )
 
-func main() {
-    resp, err := http.Get("http://example.com/")
-    if err != nil {
-        fmt.Println("Error:", err)
-        os.Exit(1)
-    }
-    defer resp.Body.Close()
-
-    fmt.Println(resp.Body)
-
-    cmd := exec.Command("notify-send", "hello")
+func clearScreen() {
+    cmd := exec.Command("clear")
+    cmd.Stdout = os.Stdout
     cmd.Run()
+}
 
-    for {
-        fmt.Println("Hello, world!")
-        time.Sleep(1 * time.Second)
-    }
-
+func main() {
     if len(os.Args) % 2 != 1 {
         fmt.Println("Please provide an even number of arguments")
         os.Exit(1)
     }
 
-    for _, arg := range os.Args[1:] {
-        fmt.Println(arg)
+    for i := 1; i < len(os.Args); i += 2 {
+        clearScreen()
+        timeUnit := "minutes"
+        if os.Args[i + 1] == "1" {
+            timeUnit = "minute"
+        }
+        notify := exec.Command("notify-send", fmt.Sprintf("%s for %s %s", os.Args[i], os.Args[i + 1], timeUnit))
+        notify.Run()
+        fmt.Println(os.Args[i])
+        minutes, err := strconv.Atoi(os.Args[i + 1])
+        if err != nil {
+            minutes = 5
+        }
+        totalSeconds := minutes * 60
+        for totalSeconds > 0 {
+            fmt.Printf("\r%02d:%02d", totalSeconds / 60, totalSeconds % 60)
+            totalSeconds -= 1
+            time.Sleep(1 * time.Second)
+        }
     }
+    fmt.Println("\nDone!")
 }
